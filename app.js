3cf2ebd8780748e9aa53e34fdbc5743d8635c6c0
kadir-zeren <k4dzeren@gmail.com>
2024-03-19 14:25:39 +0300

    float int

---
+++ app.js
@@ -0,0 +1,36 @@
+// let sayi = Number(prompt("Bir sayi giriniz:"));
+// console.log(typeof sayi);
+// let sayi2 = 10;
+// sayi = sayi + sayi2;
+
+// console.log(sayi++);
+
+console.log(Number("123"));
+console.log(Number("12.3"));
+console.log(Number("0"));
+console.log(Number(" "));
+console.log(Number(""));
+console.log(Number(null));
+console.log(Number(undefined));
+
+// const myNumber = "threezeroone";
+// let myNumber2 = Number(myNumber);
+// console.log(Number(myNumber));
+// console.log(typeof myNumber2);
+
+const dolar = "1000";
+const tl = "2000";
+
+const totalMoney = Number(dolar) + +tl;
+console.log(totalMoney);
+
+const dolarInt = parseInt(dolar);
+console.log(dolarInt);
+const tlFloat = parseFloat(tl);
+console.log(tlFloat);
+
+console.log(parseFloat(null));
+console.log(parseFloat(""));
+
+const myNumber3 = 2000.5;
+console.log(String(myNumber3));
